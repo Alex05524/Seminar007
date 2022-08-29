98f225406919cf607aa84448328482f77d03e17d
Alexey <pwz-and_10@mail.ru>
2022-08-29 21:45:05 +0600

    create task3

---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        int[,] array = new int[random.Next(1, 10), random.Next(1, 10)];
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                array[i, j] = random.Next(1, 10);
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Среднее арифметическое:");
+        for (int j = 0; j < 4; j++)
+        {
+            double sum = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                sum += array[i, j];
+            }
+            Console.Write($"{sum / 4}{";"} ");
+        }
+    }
+}
