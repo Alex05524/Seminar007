dcedfcc07c0a09f37bec0eb0024ddd3943a18e9e
Alexey <pwz-and_10@mail.ru>
2022-08-29 20:30:50 +0600

    create task2

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] TwoDimensionalArray = new int[3, 4];
+        Random ran = new Random();
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                TwoDimensionalArray[i, j] = ran.Next(1, 17);
+                Console.Write("{0}\t", TwoDimensionalArray[i, j]);
+            }
+            Console.WriteLine();
+        }
+        void ReturnNumb()
+        {
+            int i = 17;
+            int j = 17;
+            if (i > 3 && j > 4)
+            {
+                Console.WriteLine($"17 -> такого числа в массиве нет");
+                return;
+            }
+        }
+        ReturnNumb();
+    }
+}
